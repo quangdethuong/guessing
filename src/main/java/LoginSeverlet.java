45bd531bf1355538bcd8622dc245dd9818931965
quangdethuong <quangnn1106@gmail.com>
2023-04-20 23:20:57 +0700

    first commit

---
+++ src/main/java/LoginSeverlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LoginSeverlet", value = "/login")
+public class LoginSeverlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
